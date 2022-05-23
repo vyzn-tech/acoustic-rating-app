d866d8b8005d1bbdff2878cdc1c9c3c18c8b56e7
Christian Lecherbauer <christian.lecherbauer@googlemail.com>
2022-05-23 07:54:18 +0200

    [VYZN-122] add eslint cli

--- .devops/docker/alias/node/build.sh
+++ .devops/docker/alias/node/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-npm install -g @nestjs/cli  --force
-npm config set unsafe-perm true
+npm install -g @nestjs/cli eslint --force
+npm config set --global unsafe-perm true
+npm config set --global update-notifier false
