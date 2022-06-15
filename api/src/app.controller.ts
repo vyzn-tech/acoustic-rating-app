d9d7b1ca4173ccfb6092f3aba32e6f9e2a7e0e4e
Christian Lecherbauer <christian.lecherbauer@googlemail.com>
2022-06-15 18:04:19 +0200

    [VYZN-126] rename Item to Component

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -13,29 +13,15 @@ import { FileInterceptor } from '@nestjs/platform-express'
 import { ApiFile } from './api-file.decorator'
 import { ApiTags } from '@nestjs/swagger'
 import { stringify } from 'csv-stringify/sync'
-import { parseCSV } from 'csv-load-sync'
 
 import {
-  ALL_PREDEFINED_TYPES,
-  Building,
-  Door,
-  Item,
-  Roof,
-  Slab,
-  Space,
-  Wall,
-  Zone,
-  PREDEFINED_TYPE_FLOOR,
   AcousticRatingCalculator,
   ExternalAcousticRatingCollection,
   ExternalAcousticRating,
-  PREDEFINED_TYPE_ROOF,
-  FlatRoof,
-  NeighbourBuilding,
 } from '../libs/acoustic-rating-calculator/src/calculator'
-import { isString } from 'lodash'
 import { createReadStream } from 'fs'
 import * as fs from 'fs'
+import CsvConverter from './utils/csv-converter'
 
 const csvMimeTypes = [
   'application/csv',
@@ -76,7 +62,6 @@ export class AppController {
     @UploadedFile() file: Express.Multer.File,
     @Response({ passthrough: true }) res,
   ): StreamableFile {
-    const records = parseCSV(file.buffer.toString())
     const externalAcousticRatings = new ExternalAcousticRatingCollection(
       new ExternalAcousticRating(62, 55),
       new ExternalAcousticRating(0, 0),
@@ -87,14 +72,14 @@ export class AppController {
       new ExternalAcousticRating(0, 0),
       new ExternalAcousticRating(0, 0),
     )
-    const items = this.buildItems(records)
+    const items = new CsvConverter().convertToComponents(file.buffer.toString())
     const calculator = new AcousticRatingCalculator(
       items,
       externalAcousticRatings,
     )
-    calculator.calculate()
+    const output = calculator.calculate()
 
-    const outputCsvAsString = stringify(items)
+    const outputCsvAsString = stringify(output, { header: true })
     const tmpFileName = Date.now() + '.csv'
     const tmpFilePath = 'tmp/' + tmpFileName
     fs.writeFileSync(tmpFilePath, outputCsvAsString)
@@ -105,145 +90,5 @@ export class AppController {
       'Content-Disposition': 'attachment filename=output.csv',
     })
     return new StreamableFile(response_file)
-  }
-
-  buildItems(rows): Item[] {
-    const items: Item[] = []
-    for (const row of rows) {
-      switch (row.Entity.toLowerCase()) {
-        case 'ifcdoor':
-          items.push(this.buildDoor(row))
-          continue
-        case 'ifcroof':
-          items.push(this.buildRoof(row))
-          continue
-        case 'ifcspace':
-          items.push(this.buildSpace(row))
-          continue
-        case 'ifcwall':
-          items.push(this.buildWall(row))
-          continue
-        case 'ifczone':
-          items.push(this.buildZone(row))
-          continue
-      }
-
-      if (row.Entity.toLowerCase() === 'ifcbuilding') {
-        if (row.OccupancyType) {
-          items.push(this.buildNeighbourBuilding(row))
-          continue
-        }
-        items.push(this.buildBuilding(row))
-      }
-
-      if (row.Entity.toLowerCase() === 'ifcslab') {
-        if (row.PredefindedType === PREDEFINED_TYPE_ROOF) {
-          items.push(this.buildFlatRoof(row))
-          continue
-        }
-        items.push(this.buildSlab(row))
-      }
-    }
-    return items
-  }
-
-  setCommonItemAttributes<T extends Item>(row, item): T {
-    let parentIds = []
-    parentIds = row.ParentIds.replace(/ /g, '').split(',')
-    item.id = row.GUID
-    item.parentIds = parentIds
-
-    return item
-  }
-
-  setComponentAttributes<T extends Item>(row, item): T {
-    item.isExternal = row.IsExternal.toLowerCase() === 'true'
-    item.celestialDirection = row.CelestialDirection
-
-    return item
-  }
-
-  buildWall(row): Wall {
-    let item: Wall = new Wall()
-    item = this.setCommonItemAttributes(row, item)
-    item = this.setComponentAttributes(row, item)
-
-    return item
-  }
-
-  buildDoor(row): Door {
-    let item: Door = new Door()
-    item = this.setCommonItemAttributes(row, item)
-    item = this.setComponentAttributes(row, item)
-
-    return item
-  }
-
-  buildRoof(row): Roof {
-    let item: Roof = new Roof()
-    item = this.setCommonItemAttributes(row, item)
-    item = this.setComponentAttributes(row, item)
-
-    return item
-  }
-
-  buildFlatRoof(row): FlatRoof {
-    let item: FlatRoof = new FlatRoof()
-    item = this.setCommonItemAttributes(row, item)
-    item = this.setComponentAttributes(row, item)
-
-    return item
-  }
-
-  buildSpace(row): Space {
-    let item: Space = new Space()
-    item = this.setCommonItemAttributes(row, item)
-    item.occupancyType = row.OccupancyType
-    item.centerOfGravityZ = parseInt(row.CenterOfGravityZ)
-
-    return item
-  }
-
-  buildSlab(row): Slab {
-    let item: Slab = new Slab()
-    item = this.setCommonItemAttributes(row, item)
-    item = this.setComponentAttributes(row, item)
-    if (
-      !isString(row.PredefinedType) ||
-      !ALL_PREDEFINED_TYPES.includes(row.PredefinedType.toUpperCase())
-    ) {
-      item.predefinedType = PREDEFINED_TYPE_FLOOR
-    } else {
-      item.predefinedType = row.PredefinedType
-    }
-
-    return item
-  }
-
-  buildBuilding(row): Building {
-    let item: Building = new Building()
-    item = this.setCommonItemAttributes(row, item)
-    item.name = row.Name
-    item.status = row.Status
-
-    return item
-  }
-
-  buildNeighbourBuilding(row): NeighbourBuilding {
-    let item: NeighbourBuilding = new NeighbourBuilding()
-    item = this.setCommonItemAttributes(row, item)
-    item.occupancyType = row.OccupancyType
-
-    return item
-  }
-
-  buildZone(row): Zone {
-    let item: Zone = new Zone()
-    item = this.setCommonItemAttributes(row, item)
-    item.name = row.Name
-    item.status = row.Status
-    item.acousticRatingLevel = row.AcousticRatingLevelReq
-
-    return item
   }
 }
