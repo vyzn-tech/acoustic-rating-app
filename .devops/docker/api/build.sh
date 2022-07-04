e7a3c7f0cccf00f04624ccb25b4274aee98192d7
Christian Lecherbauer <christian.lecherbauer@googlemail.com>
2022-07-04 07:58:49 +0200

    [VYZN-150] remove unused mysql database service

--- .devops/docker/api/build.sh
+++ .devops/docker/api/build.sh
@@ -4,9 +4,6 @@ set -e
 SOURCE_ROOT="/var/source"
 $SOURCE_ROOT/.devops/devsh/switch_user.sh "node"
 
-apt-get -qq update
-apt-get -qq install default-mysql-client procps -y
-
 cp "$SOURCE_ROOT/.devops/docker/api/config/entrypoint.sh" /usr/local/bin
 
 #cleanup
