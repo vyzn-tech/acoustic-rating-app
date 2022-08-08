70d4ed42586510ec4dda896122c234bfee956fa4
Christian Lecherbauer <christian.lecherbauer@googlemail.com>
2022-08-08 13:48:46 +0200

    [VYZN-372] move seestrasse mappings from lib to here

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -24,9 +24,21 @@ import {
 } from '../libs/lib-acoustic-rating/src/external-acoustic-rating'
 import CsvConverter from '../libs/lib-acoustic-rating/src/csv-converter'
 import {
+  NOISE_EXPOSURE_HIGH,
+  NOISE_EXPOSURE_LOW,
+  NOISE_EXPOSURE_MODERATE,
+  NOISE_EXPOSURE_VERY_HIGH,
+  NoiseExposureMap,
   SPECTRUM_ADJUSTMENT_TYPE_C,
   SPECTRUM_ADJUSTMENT_TYPE_CTR,
+  SpectrumAdjustmentTypeMap,
 } from '../libs/lib-acoustic-rating/src/noise-exposure'
+import {
+  NOISE_SENSITIVITY_LOW,
+  NOISE_SENSITIVITY_MODERATE,
+  NOISE_SENSITIVITY_NONE,
+  NoiseSensitivityMap,
+} from '../libs/lib-acoustic-rating/src/noise-sensitivity'
 
 const csvMimeTypes = [
   'application/csv',
@@ -133,11 +145,68 @@ export class AppController {
       ),
       request.query['external-acoustic-ratings'],
     )
+    const additionalNoiseSensitivityMap: NoiseSensitivityMap = {
+      Terrasse: NOISE_SENSITIVITY_NONE,
+      Waschraum: NOISE_SENSITIVITY_NONE,
+      Einfahrt: NOISE_SENSITIVITY_NONE,
+      Keller: NOISE_SENSITIVITY_NONE,
+      Technik: NOISE_SENSITIVITY_NONE,
+      Tiefgarage: NOISE_SENSITIVITY_NONE,
+      Treppenhaus: NOISE_SENSITIVITY_NONE,
+      Gewerbe: NOISE_SENSITIVITY_LOW,
+      Bad: NOISE_SENSITIVITY_LOW,
+      Wohnen: NOISE_SENSITIVITY_MODERATE,
+      Schlafen: NOISE_SENSITIVITY_MODERATE,
+    }
+
+    const additionalAirborneNoiseExposureMap: NoiseExposureMap = {
+      Keller: NOISE_EXPOSURE_LOW,
+      Wohnen: NOISE_EXPOSURE_MODERATE,
+      Schlafen: NOISE_EXPOSURE_MODERATE,
+      Bad: NOISE_EXPOSURE_MODERATE,
+      Tiefgarage: NOISE_EXPOSURE_MODERATE,
+      Einfahrt: NOISE_EXPOSURE_MODERATE,
+      Terrasse: NOISE_EXPOSURE_MODERATE,
+      Treppenhaus: NOISE_EXPOSURE_MODERATE,
+      Waschraum: NOISE_EXPOSURE_HIGH,
+      Technik: NOISE_EXPOSURE_HIGH,
+      Gewerbe: NOISE_EXPOSURE_VERY_HIGH,
+    }
+    const additionalFootstepNoiseExposureMap: NoiseExposureMap = {
+      Keller: NOISE_EXPOSURE_LOW,
+      Wohnen: NOISE_EXPOSURE_MODERATE,
+      Schlafen: NOISE_EXPOSURE_MODERATE,
+      Bad: NOISE_EXPOSURE_MODERATE,
+      Tiefgarage: NOISE_EXPOSURE_MODERATE,
+      Einfahrt: NOISE_EXPOSURE_MODERATE,
+      Treppenhaus: NOISE_EXPOSURE_MODERATE,
+      Terrasse: NOISE_EXPOSURE_MODERATE,
+      Waschraum: NOISE_EXPOSURE_VERY_HIGH,
+      Technik: NOISE_EXPOSURE_LOW,
+      Gewerbe: NOISE_EXPOSURE_HIGH,
+    }
+    const additionalSpectrumAdjustmentTypeMap: SpectrumAdjustmentTypeMap = {
+      Keller: SPECTRUM_ADJUSTMENT_TYPE_C,
+      Wohnen: SPECTRUM_ADJUSTMENT_TYPE_C,
+      Schlafen: SPECTRUM_ADJUSTMENT_TYPE_C,
+      Bad: SPECTRUM_ADJUSTMENT_TYPE_C,
+      Tiefgarage: SPECTRUM_ADJUSTMENT_TYPE_CTR,
+      Einfahrt: SPECTRUM_ADJUSTMENT_TYPE_CTR,
+      Terrasse: SPECTRUM_ADJUSTMENT_TYPE_C,
+      Treppenhaus: SPECTRUM_ADJUSTMENT_TYPE_C,
+      Waschraum: SPECTRUM_ADJUSTMENT_TYPE_CTR,
+      Technik: SPECTRUM_ADJUSTMENT_TYPE_C,
+      Gewerbe: SPECTRUM_ADJUSTMENT_TYPE_CTR,
+    }
 
     const items = new CsvConverter().convertToComponents(file.buffer.toString())
     const calculator = new AcousticRatingCalculator(
       items,
       externalAcousticRatings,
+      additionalNoiseSensitivityMap,
+      additionalAirborneNoiseExposureMap,
+      additionalFootstepNoiseExposureMap,
+      additionalSpectrumAdjustmentTypeMap,
     )
     return calculator.calculate()
   }
