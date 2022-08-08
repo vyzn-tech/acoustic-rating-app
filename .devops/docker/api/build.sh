f548f7350b7f3016e1687262ececb40aee9bf9bf
Christian Lecherbauer <christian.lecherbauer@googlemail.com>
2022-08-08 12:28:39 +0200

    [VYZN-325] implement azure deployment

--- .devops/docker/api/build.sh
+++ .devops/docker/api/build.sh
@@ -2,8 +2,18 @@
 set -e
 
 SOURCE_ROOT="/var/source"
+WEB_ROOT="/app"
 
 cp "$SOURCE_ROOT/.devops/docker/api/config/entrypoint.sh" /usr/local/bin
+
+if [ "$PRODUCTION_BUILD" == "true" ]; then
+    (
+        cp -r "$SOURCE_ROOT/api" "$WEB_ROOT"
+        cd "$WEB_ROOT" || exit
+        npm install --unsafe-perm
+        chown -Rf node:node "$WEB_ROOT"
+    )
+fi
 
 #cleanup
 rm -Rf "$SOURCE_ROOT"
